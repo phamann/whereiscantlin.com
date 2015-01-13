1a795ae2cc5f3a2de36e493280ad2f022929a053
Patrick Hamann <patrickhamann@gmail.com>
2015-01-13 14:10:48 +0000

    Whitespace

--- static/js/whereabouts.js
+++ static/js/whereabouts.js
@@ -9,18 +9,18 @@
 		'project managing',
 		'having a cigarette',
 		'having a coffee and quinoa in a startup restaurant',
-        'watching an art house film in a rooftop cinema',
-        'in Dalston seeing a band he read about in Pitchfork Media',
-        'writing Ruby',
-        'in Brooklyn eating a ramen burger',
-        'at a pop-up restaurant in a bathroom in Walthamstow',
-        'at Lidl buying ironically unethical Brussels sprouts',
-        'in Peckham, like everybody else',
-        'wearing a suit and tie in the office',
-        'campaigning for the Labour party in an Armani suit',
-        'preening his beard',
-        'applying for Alan\'s job',
-        'in an artisan tailors purchasing a foppish yet sophisticated new suit'
+        	'watching an art house film in a rooftop cinema',
+        	'in Dalston seeing a band he read about in Pitchfork Media',
+        	'writing Ruby',
+        	'in Brooklyn eating a ramen burger',
+        	'at a pop-up restaurant in a bathroom in Walthamstow',
+        	'at Lidl buying ironically unethical Brussels sprouts',
+        	'in Peckham, like everybody else',
+        	'wearing a suit and tie in the office',
+        	'campaigning for the Labour party in an Armani suit',
+        	'preening his beard',
+        	'applying for Alan\'s job',
+        	'in an artisan tailors purchasing a foppish yet sophisticated new suit'
 	];
 
     function getRandomInt(min, max) {
