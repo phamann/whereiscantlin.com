101ed7b30774b48d0afba1e217417b96bb8e73c1
Robert Rees <robert.rees@guardian.co.uk>
2015-01-13 11:03:27 +0000

    Adds more possibilities

--- static/js/whereabouts.js
+++ static/js/whereabouts.js
@@ -8,22 +8,25 @@
 		'project managing',
 		'having a cigarette',
 		'having a coffee and quinoa in a startup restaurant',
-                'watching an art house film in a rooftop cinema',
-                'in Dalston seeing a band he read about in Pitchfork Media',
-                'writing Ruby',
-                'in Brooklyn eating a ramen burger',
-                'at a pop-up restaurant in a bathroom in Walthamstow',
-                'at Lidl buying ironically unethical Brussels sprouts',
-                'in Peckham, like everybody else'
+		'watching an art house film in a rooftop cinema',
+		'in Dalston seeing a band he read about in Pitchfork Media',
+		'writing Ruby',
+		'in Brooklyn eating a ramen burger',
+		'at a pop-up restaurant in a bathroom in Walthamstow',
+		'at Lidl buying ironically unethical Brussels sprouts',
+		'in Peckham, like everybody else',
+		'presenting something in morning conference',
+		're-cutting his student films',
+		'promising things to people'
 	];
 
 	function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
+		return Math.floor(Math.random() * (max - min)) + min;
+	}
 
-    function getRandomLocation() {
-        return WHEREABOUTS[getRandomInt(0, WHEREABOUTS.length)];
-    }
+	function getRandomLocation() {
+		return WHEREABOUTS[getRandomInt(0, WHEREABOUTS.length)];
+	}
 
 	document.querySelector('.js-whereabouts').innerHTML = getRandomLocation();
 })();
